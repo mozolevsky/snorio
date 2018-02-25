f82b63635d1dfabe9293140eb7a9153d25a01459
mozolevsky <alf.rulit@gmail.com>
2018-02-25 16:11:08 +0300

    Merged client version

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -17,37 +17,37 @@ let slideout = new Slideout({
     'padding': 256,
     'tolerance': 70
   });
-    
+
   // Toggle button
     document.querySelectorAll('.js-slideout-toggle').forEach(function(item) {
         item.addEventListener('click', function() {
           slideout.toggle();
       });
     });
-  
+
     let fixed = document.querySelector('.header-sticky');
-  
+
     slideout.on('translate', function(translated) {
         fixed.style.transform = 'translateX(' + translated + 'px)';
     });
-  
+
     slideout.on('beforeopen', function () {
         fixed.style.transition = 'transform 300ms ease';
         fixed.style.transform = 'translateX(256px)';
         fixed.classList.add("header-sticky_with-overlay");
     });
-  
+
     slideout.on('beforeclose', function () {
         fixed.style.transition = 'transform 300ms ease';
         fixed.style.transform = 'translateX(0px)';
         fixed.classList.remove("header-sticky_with-overlay");
     });
-  
+
     function close(eve) {
       eve.preventDefault();
       slideout.close();
     }
-    
+
     slideout
       .on('beforeopen', function() {
         this.panel.classList.add('panel-open');
@@ -62,14 +62,12 @@ let slideout = new Slideout({
 
 
   // timer
-if ($('.clock')) {
-    let clock = $('.clock').FlipClock(3600, {
-        countdown: true
-    });
-}
+let clock = $('.clock').FlipClock(3600, {
+    countdown: true
+});
 
 
-//reviews 
+//reviews
 let reviewsBtn = document.querySelector('.reviews__btn-js');
 let reviewsBtnText = document.querySelector('.reviews__btn-text-js');
 let reviewsSpinner = document.querySelector('.loader');
@@ -78,26 +76,26 @@ let counter = 0;
 if (reviewsBtn && reviewsBtnText && reviewsSpinner) {
     reviewsBtn.addEventListener('click', (e) => {
         e.preventDefault();
-    
-        if (counter < 2) {
+
+        if (counter < 10) {
             reviewsBtnText.classList.toggle('d-none');
             reviewsBtn.setAttribute('disabled', true);
             reviewsSpinner.classList.toggle('d-none');
             let reviewsHiddenBlock = document.querySelector(`.reviews-${counter}-js`);
-            
+
             ++counter;
-    
+
             setTimeout(() => {
                 reviewsSpinner.classList.toggle('d-none');
-    
-                if (counter === 2) {
+
+                if (counter === 10) {
                     reviewsBtn.setAttribute('disabled', true);
-                    reviewsBtn.innerText = 'All posts loaded';
+                    reviewsBtn.innerText = 'All comments loaded';
                 } else {
                     reviewsBtn.removeAttribute('disabled');
                     reviewsBtnText.classList.toggle('d-none');
                 }
-    
+
                 reviewsHiddenBlock.classList.remove('d-none');
             }, 2500);
         }
